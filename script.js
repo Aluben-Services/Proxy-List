7d0b9c2db5e8838ce6251e53f7276c1054ea8a27
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-09-14 14:46:35 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function Light() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    button.style.color = "white";
+    button.style.backgroundColor = "black";
+  }
+
+
+  function Dark() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    button.style.color = "black";
+    button.style.backgroundColor = "white";
+  }
+
+
+
+  function butt() {
+    var win = window.open(); var url = 'https://sciencenow.info'; var iframe = win.document.createElement('iframe'); iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
+  }
+
+
+
+  function raw() {
+    var win = window.open(); var url = 'https://teachers.beauty'; var iframe = win.document.createElement('iframe'); iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
+  }
+
+  function tub() {
+    var win = window.open(); var url = 'https://k12-fl.k12sciencemuseum.org'; var iframe = win.document.createElement('iframe'); iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
+  } 
+
+
+
+  function both() {
+    var win = window.open(); var url = 'https://interactive.kydana.art'; var iframe = win.document.createElement('iframe'); iframe.style = "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;"; iframe.src = url; win.document.body.appendChild(iframe);
+  }
+
+
+function discord() {
+  window.open('https://discord.gg/Jeybsy2us4')
+}
